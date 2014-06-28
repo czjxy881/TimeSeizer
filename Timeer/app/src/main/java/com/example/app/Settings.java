1a5a5f64865a0d402882a3b8abf5457296b4e40b
jxy <czjxy8898@gmail.com>
2014-06-29 00:16:32 +0800

    设置栏布局

--- Timeer/app/src/main/java/com/example/app/Settings.java
+++ Timeer/app/src/main/java/com/example/app/Settings.java
@@ -2,15 +2,21 @@ package com.example.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by xxx on 14-6-10.
  */
 public class Settings extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_layout);
+
     }
+
 
 }
