e298ef20c856cc0f1c4efe9a23f1af1d272e1058
jxy <czjxy8898@gmail.com>
2014-07-01 03:19:12 +0800

    Revert 4b0e7bb..7863b5a
    This rolls back to commit 4b0e7bb8594927380cae51d6b81da84783ea4f50.

--- Timeer/app/src/main/java/com/example/app/LeftFragment.java
+++ Timeer/app/src/main/java/com/example/app/LeftFragment.java
@@ -1,6 +1,5 @@
 package com.example.app;
 
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
@@ -20,8 +19,6 @@ public class LeftFragment extends Fragment {
     private TextView tvfqnumber;
     private TextView tvddnumber;
     private EditText etsummary;
-    private Button summary;
-    private Dialog dialog;
     private static LeftFragment leftFragment=null;
 
 
@@ -56,7 +53,6 @@ public class LeftFragment extends Fragment {
             tvddnumber=(TextView)getActivity().findViewById(R.id.tvddnumber);
             rbmysocre = (RatingBar)getActivity().findViewById(R.id.rbmyscore);
             btnpingjia = (Button)getActivity().findViewById(R.id.btnpingjia);
-            summary = (Button)getActivity().findViewById(R.id.Summary);
 
             //RatingBar的设置
             rbmysocre.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
@@ -70,47 +66,18 @@ public class LeftFragment extends Fragment {
                 @Override
                 public void onClick(View view) {
                     //TODO::完成完成评价的按钮的设置
-
                 }
             });
 
-            summary.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    SummaryDialog();
-                }
-            });
         }
     }
 
-
-
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+
+
+
     }
 
-
-    private void SummaryDialog(){
-        dialog = new Dialog(getActivity(), R.style.mydialog);
-        dialog.setContentView(R.layout.alertdialog);
-        final EditText editText = (EditText)dialog.findViewById(R.id.mysummary);
-        //editText.setText(""+"\n"+"");
-        dialog.findViewById(R.id.DialogSaveButton2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO::保存评价
-
-                String str=editText.getText().toString();
-                //Toast.makeText(getActivity(),str,Toast.LENGTH_SHORT).show();
-                dialog.cancel();
-            }
-        });
-        dialog.findViewById(R.id.DialogCancelButton2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.cancel();
-            }
-        });
-        dialog.show();
-    }
 }
